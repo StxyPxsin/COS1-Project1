da82f7afacdc55bdcae813f8de4915d2c10d3af2
Ariel Mauricio <AAMauricio@student.fullsail.edu>
2026-09-04 11:19:30 -0500

    added version control

--- dev/AceZombie Survival/AceZombie Survival/GameBoard.h
+++ dev/AceZombie Survival/AceZombie Survival/GameBoard.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <string>
 class GameBoard
 {
+public:
+	static const int Width = 50;
+	static const int height= 22;
+	static const int FrameDelayMs = 33;
+	
+};
+
+struct Position {
+	double x;
+	double y;
+	void bound() {
+		if (x < 0) x = 0;
+		if (x >= GameBoard::height) x = GameBoard::Width - 1;
+	}
 };
 
